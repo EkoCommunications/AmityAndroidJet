4537316082b2e597259183984cf523e400071c97
Pan <theptai@ekoapp.com>
2021-07-19 17:29:01 +0700

    Update SampleFragment.kt

--- app/src/main/java/co/amity/android/viewbinding/SampleFragment.kt
+++ app/src/main/java/co/amity/android/viewbinding/SampleFragment.kt
@@ -2,23 +2,24 @@ package co.amity.android.viewbinding
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import co.amity.android.databinding.FragmentSampleBinding
 import co.amity.presentation.ViewBindingFragment
 
 class SampleFragment : ViewBindingFragment<FragmentSampleBinding>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun generateViewBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentSampleBinding {
+        return FragmentSampleBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         binding.fragmentSampleTextView.text = "hello world!"
 
         binding.fragmentSampleButton.setOnClickListener {
 
         }
-    }
-
-    override fun generateViewBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentSampleBinding {
-        return FragmentSampleBinding.inflate(inflater, container, false)
     }
 }

0a674c751d7a3a2f617b3d1e0b76327a9aaad1f9
Pan <theptai@ekoapp.com>
2021-08-19 13:24:23 +0700

    Update BookPageKeyedRxRemoteMediator.kt

--- app/src/main/java/co/amity/android/rxremotemediator/BookPageKeyedRxRemoteMediator.kt
+++ app/src/main/java/co/amity/android/rxremotemediator/BookPageKeyedRxRemoteMediator.kt
@@ -38,7 +38,7 @@ class BookPageKeyedRxRemoteMediator(private val title: String, private val categ
                                 category = category,
                                 next = it.get("next").asString,
                                 previous = null,
-                                uniqueIds = books.map { book -> book.asJsonObject["id"].asString }
+                                ids = books.map { book -> book.asJsonObject["id"].asString }
                             )
                         )
                     )
@@ -59,7 +59,7 @@ class BookPageKeyedRxRemoteMediator(private val title: String, private val categ
                                 category = category,
                                 next = it.get("next").asString,
                                 previous = it.get("previous").asString,
-                                uniqueIds = books.map { book -> book.asJsonObject["id"].asString }
+                                ids = books.map { book -> book.asJsonObject["id"].asString }
                             )
                         )
                     )
